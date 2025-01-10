30855c8b1797a03cbbcdc0cc3d07f943ee554cb7
mtalha218 <muhammadtalha218218@gmail,com>
2025-01-10 16:22:13 +0500

    Setup changes 1st time

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -10,4 +10,28 @@ config.resolve.modules = [
 
 config.module.rules[0].exclude = /node_modules\/(?!(query-string|split-on-first|strict-uri-encode|@edx))/;
 
+// Add comprehensive warning filters
+config.stats = {
+  ...config.stats,
+  warningsFilter: [
+    // Filter legacy JS API warnings
+    /Deprecation The legacy JS API is deprecated/,
+    
+    // Filter Dart Sass 3.0.0 warnings about global built-in functions
+    /Global built-in functions are deprecated/,
+    
+    // Filter Sass @import warnings
+    /Sass @import rules are deprecated/,
+    
+    // Filter specific paragon warnings
+    /@openedx\/paragon\/scss\/core\/_exports\.module\.scss/,
+    
+    // Filter map-get warnings
+    /Use map\.get instead/,
+    
+    // Filter repetitive warnings
+    /repetitive deprecation warnings omitted/
+  ]
+};
+
 module.exports = config;
